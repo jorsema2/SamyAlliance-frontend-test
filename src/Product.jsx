578d5ca1494b0c9e878a2b9e249f3907e39ced45
jorsema2 <jorsema2@gmail.com>
2025-02-08 17:33:21 +0100

    Added like count persistance using GraphQL mutation

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
+
+export const LIKE_IMAGE = gql`
+  mutation LikeImage($input: LikeImageInput!) {
+    likeImage(input: $input) {
+      image {
+        id
+        liked
+        likesCount
+      }
+    }
+  }
+`;
 
 const Product = ({ product }) => {
-  const [isLiked, setIsLiked] = useState(product.liked);
+  const [likeImage, { data, loading, error }] = useMutation(LIKE_IMAGE);
 
   return (
     <div style={{ border: "1px solid blue", padding: "10px", margin: "10px" }}>
       <div>{product.price}.00€</div>
       <img src={product.picture} />
       <div>
-        <button onClick={() => setIsLiked(!isLiked)}>
-          {isLiked ? "❤️" : "♡"} {product.likesCount}
+        <button
+          onClick={() =>
+            likeImage({
+              variables: {
+                input: {
+                  imageId: product.id,
+                },
+              },
+            })
+          }
+        >
+          {product.liked ? "❤️" : "♡"} {product.likesCount}
         </button>
         <button>Send</button>
       </div>
