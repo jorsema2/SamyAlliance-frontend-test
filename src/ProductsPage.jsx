b5ad375cf8fe1557354fa64b978ccc94bcee5ad5
jorsema2 <jorsema2@gmail.com>
2025-02-08 14:30:09 +0100

    Added pagination prototype

--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -5,8 +5,8 @@ import Header from "./Header";
 import ProductsList from "./ProductsList";
 
 const GET_PRODUCTS = gql`
-  query GetImages {
-    images {
+  query GetImages($first: Int, $after: String) {
+    images(first: $first, after: $after) {
       edges {
         node {
           id
@@ -29,17 +29,42 @@ const ProductsPage = () => {
   const [searchQuery, setSearchQuery] = useState(
     "You're looking for something?"
   );
-  // const [products, setProducts] = useState();
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
+  const { loading, error, data, fetchMore } = useQuery(GET_PRODUCTS, {
+    variables: {
+      first: 12,
+    },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-  
+
   return (
     <div>
       <p>{searchQuery}</p>
       <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <ProductsList products={data.images.edges} />
+      <button
+        onClick={() => {
+          fetchMore({
+            variables: {
+              after: data.images.pageInfo.endCursor,
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+              return {
+                images: {
+                  ...fetchMoreResult.images,
+                  edges: [
+                    ...prev.images.edges,
+                    ...fetchMoreResult.images.edges,
+                  ],
+                },
+              };
+            },
+          });
+        }}
+      >
+        Fetch more
+      </button>
     </div>
   );
 };
